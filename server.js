cf5749c1c75601d0fc89c4822fc104aa02fbe629
Diana Baranova <Diana_Baranova@epam.com>
2017-10-19 15:29:55 +0300

    mongodb connection

--- server.js
+++ server.js
@@ -38,6 +38,12 @@ app.get('/about',(req, res) => {
     });
 });
 
+app.get('/test',(req, res) => {
+    res.send('hello world');
+});
+
 app.listen(port, function () {
     console.log(`Example app listening on port ${port}!`)
 });
+
+module.exports.app = app;
