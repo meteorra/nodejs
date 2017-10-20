55da2bb158477b6e3d63ed62da0d05fbfb4a36c6
Diana Baranova <Diana_Baranova@epam.com>
2017-10-20 15:57:21 +0300

    get /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID} = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -27,6 +28,22 @@ app.post('/todos', (req, res) => {
     }, (err) => {
         res.status(400).send(err);
     })
+});
+
+app.get('/todos/:id', (req, res) => {
+
+    if(!ObjectID.isValid(req.params.id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(req.params.id).then((todo) => {
+        if(!todo) {
+            return res.status(404).send();
+        }
+        res.send({ todo });
+    }, (err) => {
+        res.status(400).send(err);
+    });
 });
 
 app.listen(3000, function () {
--- server/server.test.js
+++ server/server.test.js
@@ -1,12 +1,15 @@
 const expect  = require('expect');
 const request = require('supertest');
+const { ObjectID} = require('mongodb');
 
 const { app } = require('./server');
 const { Todo } = require('./models/todo');
 
 const todos = [{
+    _id: new ObjectID(),
     text: 'First test todo'
 }, {
+    _id: new ObjectID(),
     text: 'Second test todo'
 }];
 
@@ -66,6 +69,38 @@ describe('GET /todos', () => {
             .expect((res) => {
                 expect(res.body.todos.length).toBe(2);
             })
+            .end(done);
+    });
+});
+
+describe('GET /todos/:id', () => {
+    it('should return a todo by id', (done) => {
+        const id = todos[0]._id;
+
+        request(app)
+            .get(`/todos/${id.toHexString()}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(todos[0].text);
+            })
+            .end(done);
+    });
+
+    it('should return a 404 if todo is not found', (done) => {
+        const id = new ObjectID();
+
+        request(app)
+            .get(`/todos/${id.toHexString()}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('should return a 404 if todoId is not valid', (done) => {
+        const id = 123;
+
+        request(app)
+            .get(`/todos/${id}`)
+            .expect(404)
             .end(done);
     });
 });
