509a72699299629efd97590c04b591811ee5f0e8
Diana Baranova <Diana_Baranova@epam.com>
2017-10-23 10:44:00 +0300

    add delete todos by id

--- server/server.js
+++ server/server.js
@@ -42,7 +42,25 @@ app.get('/todos/:id', (req, res) => {
             return res.status(404).send();
         }
         res.send({ todo });
-    }, (err) => {
+    }).catch((err) => {
+        res.status(400).send(err);
+    });
+});
+
+app.delete('/todos/:id', (req, res) => {
+
+    const id = req.params.id;
+
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if(!todo) {
+            return res.status(404).send();
+        }
+        res.send({ todo });
+    }).catch((err) => {
         res.status(400).send(err);
     });
 });
