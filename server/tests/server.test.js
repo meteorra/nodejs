bdf470f3d1c53867bccb8d5c90c1e699c9bc86e7
Diana Baranova <Diana_Baranova@epam.com>
2017-10-24 16:39:59 +0300

    login route test

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -254,3 +254,52 @@ describe('POST /users', () => {
             .end(done);
     });
 });
+
+describe('POST /users/login', () => {
+    it('should login user and return auth token', (done) => {
+
+        const email = users[1].email;
+        const password = users[1].password;
+
+        request(app)
+            .post(`/users/login`)
+            .send({ email, password})
+            .expect(200)
+            .expect((res) => {
+                expect(typeof res.headers['x-auth']).toBe('string');
+            })
+            .end((err, res) => {
+                if(err) {
+                    return done(err);
+                }
+
+                User.findById(users[1]._id).then((user) => {
+                    expect(user.tokens[0].token).toBe(res.headers['x-auth']);
+                    done();
+                }).catch((e) => done(e))
+            });
+    });
+
+    it('should reject invalid login', () => {
+        const email = users[1].email;
+        const password = 'login123';
+
+        request(app)
+            .post(`/users/login`)
+            .send({ email, password})
+            .expect(400)
+            .expect((res) => {
+                expect(typeof res.headers['x-auth']).toBe('undefined');
+            })
+            .end((err, res) => {
+                if(err) {
+                    return done(err);
+                }
+
+                User.findById(users[1]._id).then((user) => {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch((e) => done(e))
+            });
+    })
+});
