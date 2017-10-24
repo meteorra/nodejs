82cbc52925fa94b9018b6b05188c6344f3108a23
Diana Baranova <Diana_Baranova@epam.com>
2017-10-24 13:04:18 +0300

    auth middleware

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const { ObjectID } = require('mongodb');
 const _ = require('lodash');
 
 const { mongoose } = require('./db/mongoose');
+const { authenticate } = require('./middleware/authenticate');
 const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
 
@@ -101,6 +102,10 @@ app.post('/users', (req, res) => {
     }).catch((err) => {
         res.status(400).send(err);
     })
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
 });
 
 app.listen(process.env.PORT, function () {
