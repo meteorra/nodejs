8e76fe6c9dc148caec56b8b02ea7b5f08379858e
Diana Baranova <Diana_Baranova@epam.com>
2017-10-24 11:08:24 +0300

    auth route

--- server/models/user.js
+++ server/models/user.js
@@ -1,11 +1,54 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-const User = mongoose.model('User', {
+const UserSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
         trim: true,
-    }
+        unique: true,
+        validate: {
+            validator: value => validator.isEmail(value),
+            message: '{VALUE} is not a valid email'
+        },
+    },
+    password: {
+        type: String,
+        require: true,
+        minlength: 6,
+    },
+    tokens: [{
+        access: {
+            type: String,
+            require: true,
+        },
+        token: {
+            type: String,
+            require: true,
+        },
+    }],
 });
+
+UserSchema.methods.toJSON = function() {
+    const user = this;
+    const userObject = user.toObject();
+    return _.pick(userObject, ['_id', 'email']);
+};
+
+UserSchema.methods.generateAuthToken = function() {
+    const user = this;
+    const access = 'auth';
+    const token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+    user.tokens.push({access, token});
+
+    return user.save().then(() => {
+        return token;
+    })
+};
+
+const User = mongoose.model('User', UserSchema);
 
 module.exports = { User };
--- server/server.js
+++ server/server.js
@@ -91,6 +91,18 @@ app.patch('/todos/:id', (req, res) => {
     });
 });
 
+app.post('/users', (req, res) => {
+    const user = new User(_.pick(req.body, ['email', 'password']));
+
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((err) => {
+        res.status(400).send(err);
+    })
+});
+
 app.listen(process.env.PORT, function () {
     console.log(`Example app listening on port ${process.env.PORT}!`)
 });
