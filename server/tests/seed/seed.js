5b742b498860a48fd34d18895ace0d9c9ca9e854
Diana Baranova <Diana_Baranova@epam.com>
2017-10-25 10:48:31 +0300

    config update

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -32,7 +32,7 @@ const users = [{
     password: 'user1pass',
     tokens: [{
         access: 'auth',
-        token: jwt.sign({_id: userOneId.toHexString(), access: 'auth'}, 'abc123').toString(),
+        token: jwt.sign({_id: userOneId.toHexString(), access: 'auth'}, process.env.JWT_SECRET).toString(),
     }]
 }, {
     _id: userTwoId,
@@ -40,7 +40,7 @@ const users = [{
     password: 'user2pass',
     tokens: [{
         access: 'auth',
-        token: jwt.sign({_id: userTwoId.toHexString(), access: 'auth'}, 'abc123').toString(),
+        token: jwt.sign({_id: userTwoId.toHexString(), access: 'auth'}, process.env.JWT_SECRET).toString(),
     }]
 }];
 
